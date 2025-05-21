2442af2c39f3afe7a37fa301870cbc2f6a7cb178
Dipanshu Sahoo <148225619+Dipanshu789@users.noreply.github.com>
2025-05-21 22:48:07 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginAdmin, registerAdmin } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/login", loginAdmin);
-router.post("/register", registerAdmin); // <-- UNCOMMENT THIS LINE
-
-export default router;
